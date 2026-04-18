592df86f421c6104e5c778ecc6bc20d69c975700
Genesis Gutierrez <Gggensis2005@gmail.com>
2026-04-17 19:19:32 -0600

    Creacion del modelo Producto con validaciones Data Annotations

---
+++ Lab04_Productos_C4F794/Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab04_Productos_C4F794.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Range(0.01, 999999, ErrorMessage = "El precio debe ser mayor a 0")]
+        public decimal Precio { get; set; }
+
+        [Required(ErrorMessage = "La categoría es obligatoria")]
+        public string Categoria { get; set; }
+    }
+}
