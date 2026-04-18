ab5ffb644bbdfd999c1f54e8aa08bfb05da37235
Gggen <Gggen@GENESIS>
2026-04-17 18:11:29 -0600

    Agregar archivos de proyecto.

---
+++ Lab04_Productos_C4F794/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab04_Productos_C4F794.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
